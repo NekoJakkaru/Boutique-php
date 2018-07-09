78ec02bdc166458506fce71d499a6fbe49cf85e1
Caroline <c.bove16@gmail.com>
2018-07-09 11:11:04 +0200

    add li + cart panier

--- js/script.js
+++ js/script.js
@@ -34,20 +34,37 @@ $(document).ready(function() {
     $(article).append(buyB);
 
 
+
   };
   // Fin creation article
   var i = GET_PARAM("product_id");
-  var myProduct = catalog[i];
+  if(i) {
+      var myProduct = catalog[i];
 
-  var productP = $('.productP');
-  var quant = $('.quantity');
-  var cost = $('.price');
+      var productP = $('.productP');
+      var quant = $('.quantity');
+      var cost = $('.price');
 
-  (productP).text(myProduct['description']);
-  (quant).text('Quantity: '+myProduct['quantity']+'');
-  (cost).text('Price: '+myProduct['price']+' cents');
+      (productP).text(myProduct['description']);
+      (quant).text('Quantity: '+myProduct['quantity']+'');
+      (cost).text('Price: '+myProduct['price']+' cents');
+
+  }
 
 
-  console.log(myProduct);
+// Panier
+
+var cart = {};
+$('.btnBuy').click(function(){
+    var listPanier = $("<li>");
+    $('#shopList').append(listPanier);
+    if (cart[i] == null) {
+        cart[i] = 1;
+        }else{
+        cart[i]++;
+    }
+console.log(cart);
+});
+
 
 });
